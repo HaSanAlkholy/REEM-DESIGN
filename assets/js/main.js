c48ca7ea8a880f631fe41d888a178a4324638c30
Hasan Alkholy <hassan.m.h.m.a123@gmail.com>
2020-08-10 03:00:21 -0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,10 +31,10 @@ function search() {
     if (form.classList.contains('show')) {
       form.classList.remove('show');
       setTimeout(() => {
-        input.style.display="none";
+        input.style.display = "none";
       }, 500);
     } else {
-      input.style.display="block";
+      input.style.display = "block";
       setTimeout(() => {
         form.classList.add('show');
       }, 0);
@@ -45,12 +45,189 @@ function search() {
 
 // second navbar
 
-$('#sec-icon').click(function (e) { 
+$('#sec-icon').click(function (e) {
   $('#sec-items').toggleClass('show');
 });
 
-$('#sec-items').click(function (e) { 
+$('#sec-items').click(function (e) {
   $('#sec-items').removeClass('show');
 });
 
 
+// map
+
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//     styles: [
+//       {
+//         "featureType": "all",
+//         "elementType": "labels.text.fill",
+//         "stylers": [
+//           {
+//             "saturation": 36
+//           },
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 40
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "all",
+//         "elementType": "labels.text.stroke",
+//         "stylers": [
+//           {
+//             "visibility": "on"
+//           },
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 16
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "all",
+//         "elementType": "labels.icon",
+//         "stylers": [
+//           {
+//             "visibility": "off"
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "administrative",
+//         "elementType": "geometry.fill",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 20
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "administrative",
+//         "elementType": "geometry.stroke",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 17
+//           },
+//           {
+//             "weight": 1.2
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "landscape",
+//         "elementType": "geometry",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 20
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "poi",
+//         "elementType": "geometry",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 21
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "road.highway",
+//         "elementType": "geometry.fill",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 17
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "road.highway",
+//         "elementType": "geometry.stroke",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 29
+//           },
+//           {
+//             "weight": 0.2
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "road.arterial",
+//         "elementType": "geometry",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 18
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "road.local",
+//         "elementType": "geometry",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 16
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "transit",
+//         "elementType": "geometry",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 19
+//           }
+//         ]
+//       },
+//       {
+//         "featureType": "water",
+//         "elementType": "geometry",
+//         "stylers": [
+//           {
+//             "color": "#000000"
+//           },
+//           {
+//             "lightness": 17
+//           }
+//         ]
+//       }
+//     ]
+//   });
+// }
